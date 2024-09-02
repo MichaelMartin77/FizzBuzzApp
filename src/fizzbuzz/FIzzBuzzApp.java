d19126f54533569696f35350fb80204b2e86b28f
Michael <martin.michael1477@gmail.com>
2024-09-01 22:41:24 -0500

    FizzBuzz Complete

--- src/fizzbuzz/FIzzBuzzApp.java
+++ src/fizzbuzz/FIzzBuzzApp.java
@@ -3,7 +3,24 @@ package fizzbuzz;
 public class FIzzBuzzApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// User story #1 - The program prints the numbers 1-100 to the console 
+		 
+		for(int i = 1; i <= 100; i++) {
+			// User Story #2 - If a number is divisible by 3 replace the numeric console output with the word "fizz"
+			if (i  % 3 == 0 && i % 5 == 0) {
+				System.out.println("fizzbuzz");
+				continue; 
+				// User Story #3 - If a number is divisible by 5 replace the numeric console output with the word “buzz”.
+			} else if (i % 3 == 0) {
+				System.out.println("fizz");
+				continue;
+				// User Story #4 - If a number is divisible by both 3 and 5 replace the numeric console output with "fizzbuzz".
+			} else if (i % 5 == 0) {
+				System.out.println("buzz");
+				continue;
+			}
+			System.out.println(i);
+		}
 
 	}
 
